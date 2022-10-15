c65a5c7fb415c7af498fef4776fd0fab8b9897c8
Valeria Balatskaya <valeriabalacka@gmail.com>
2022-10-15 18:03:32 +0200

    added git url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        Created by Valeria Balatska and is{" "}
+      <a href="https://github.com/Valeria-Balatskaya/react-weather" target="blank">open-sourced</a>
+      </footer>
     </div>
   );
 }
