107c44abb3c1d91ee628a162cd26b1466b8e9878
Valeria Balatskaya <valeriabalacka@gmail.com>
2022-10-15 18:21:01 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div class="container">
       <h1>Weather App</h1>
       <footer>
         Created by Valeria Balatska and is{" "}
       <a href="https://github.com/Valeria-Balatskaya/react-weather" target="blank">open-sourced</a>
       </footer>
+      </div>
     </div>
   );
 }
