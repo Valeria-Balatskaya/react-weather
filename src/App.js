af03ab25bd009ae746ea450752098e372918ebd9
Valeria Balatskaya <valeriabalacka@gmail.com>
2022-10-23 18:42:41 +0200

    added CSS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
 import './App.css';
 import Weather from "./Weather"
+import image from "./img/background.jpg"
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="page">
+    <div className="App" style={{ backgroundImage:`url(${image})`, backgroundRepeat:"no-repeat",backgroundSize:"cover", borderRadius:14, height:400}}>
       <div class="container">
       <Weather defaultCity="Kyiv" />
-      <footer>
-        Created by Valeria Balatska and is{" "}
-      <a href="https://github.com/Valeria-Balatskaya/react-weather" target="blank">open-sourced</a>
-      </footer>
       </div>
+      </div>
+      <footer className="bottom">
+        Created by Valeria Balatska and is{" "}
+      <a href="https://github.com/Valeria-Balatskaya/react-weather" target="blank" >open-sourced</a>
+      </footer>
       </div>
   );
 }
